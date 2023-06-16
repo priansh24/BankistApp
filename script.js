eee225c6e42f358a4957432e431422df7ab618c7
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2023-06-16 13:09:00 +0530

    Update script(added my friends names).js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Data
 const account1 = {
-    owner: 'Jonas Schmedtmann',
+    owner: 'Tanmay Sachan',
     movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
     interestRate: 1.2, // %
     pin: 1111,
@@ -26,7 +26,7 @@ const account1 = {
 };
 
 const account2 = {
-    owner: 'Jessica Davis',
+    owner: 'Tarang Verma',
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
@@ -47,7 +47,7 @@ const account2 = {
 
 
 const account3 = {
-    owner: 'Steven Thomas Williams',
+    owner: 'Yagay Khatri',
     movements: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
     pin: 3333,
@@ -66,7 +66,7 @@ const account3 = {
 };
 
 const account4 = {
-    owner: 'Sarah Smith',
+    owner: 'Priyansh Singhal',
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 4444,
